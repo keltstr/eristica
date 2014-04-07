818c4544f67e32dfc0d40b7bdf7c339ef6f8b35f
Vitaly Iegorov <egorov@samsonos.com>
2014-04-07 17:42:43 +0300

    Removed old data

---
+++ app/controller/main.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
+ * on 07.04.14 at 17:40
+ */
+function main()
+{
+    m()->view('main')->title('Главная');
+}
--- app/view/main.php
+++ app/view/main.php
@@ -3,4 +3,5 @@
  * Created by Vitaly Iegorov <egorov@samsonos.com>
  * on 01.04.14 at 18:34
  */
-  
+?>
+<h1>Главная</h1> 
